4b50c7be4c3c279c3de9c5d3f20a4f81a01fab1b
Trevor Fountain <doches@gmail.com>
2011-08-11 00:15:56 +0100

    UNICODE SUPPORT FUCK YEAH

--- FontTool.py
+++ FontTool.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=UTF8
 import os
 import sys
 from optparse import OptionParser
@@ -8,8 +9,9 @@ parser.add_option("-f","--font",dest="font",help="(REQUIRED) Name of the font to
 parser.add_option("-s","--size",dest="size",type="int",help="Desired font size (in points). Defaults to 64.",default=64)
 parser.add_option("--surface",dest="surface_size",help="Size of the surface into which to render, of the form WIDTHxHEIGHT). Defaults to 512x512.",default="512x512")
 parser.add_option("-c","--color",dest="color",help="Color to render, of the form RRR,GGG,BBB. Defaults to 255,255,255.",default="255,255,255")
-parser.add_option("--chars",dest="characters",help="List of characters to include. Defaults to a reasonable list of mixed case alphanumerics and punctuation.",default="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.:!?@#$%^&*()[]\"\'-/<>{}+;")
+parser.add_option("--chars",dest="characters",help="List of characters to include. Defaults to a reasonable list of mixed case alphanumerics and punctuation.",default=unicode("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.:!?@#$^&*()[]\"\'-/<>{}+;","UTF8","strict"))
 parser.add_option("--plist",action="store_true",dest="plist",default=False,help="Flag indicating that we want the font information in Apple plist format (for loading with NSDictionary).")
+parser.add_option("-u","--unicode",action="store_true",dest="unicode",default=False,help="Include bonus unicode characters.")
 parser.add_option("--no-aa",action="store_false",dest="antialias",default=True,help="Disable anti-aliasing")
 
 (options,parser) = parser.parse_args()
@@ -29,6 +31,8 @@ surfsize = (int(surfsize[0]),int(surfsize[1]))
 color = options.color.split(",")
 color = (int(color[0]),int(color[1]),int(color[2]))
 chars = options.characters
+if options.unicode:
+  chars = chars + unicode("%éüùú£¢¡¿àáñöóòïíèáéíóúüñ","UTF8")
 image_filename = font+"-"+str(fontsize)+".png"
 output = "font"
 if options.plist:
@@ -67,7 +71,7 @@ for c in chars:
 		if output == "plist":
 			if c in escapes:
 				c = escapes[c]
-			fout.write("\t<key>" + c + "</key>\n")
+			fout.write("\t<key>" + c.encode("utf-8") + "</key>\n")
 			fout.write("\t<array>\n")
 			for v in [x,surfsize[1]-y,charsize[0],charsize[1]]:
 				fout.write("\t\t<integer>"+str(v)+"</integer>\n")
